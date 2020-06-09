f6d68e3f4b8fa979116ca04fbcf4b6ef0eb3596d
smappinc <46318200+smappinc@users.noreply.github.com>
2020-06-09 18:52:09 +0300

    Sgnl Code

--- app/src/main/java/org/thoughtcrime/securesms/conversationlist/ConversationListFragment.java
+++ app/src/main/java/org/thoughtcrime/securesms/conversationlist/ConversationListFragment.java
@@ -68,6 +68,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.annimon.stream.Stream;
 import com.google.android.material.snackbar.Snackbar;
 
+import com.facebook.ads.Ad;
+import com.facebook.ads.AdError;
+import com.facebook.ads.AdIconView;
+import com.facebook.ads.NativeAdLayout;
+import com.facebook.ads.NativeAdListener;
+import com.facebook.ads.NativeBannerAd;
+import com.facebook.ads.NativeBannerAdView;
+
+import android.widget.LinearLayout;
+import android.widget.Button;
+
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
@@ -174,6 +185,9 @@ public class ConversationListFragment extends MainFragment implements LoaderMana
   private ConversationListSearchAdapter searchAdapter;
   private StickyHeaderDecoration        searchAdapterDecoration;
   private ViewGroup                     megaphoneContainer;
+  private NativeAdLayout                nativeAdLayout;
+  private NativeBannerAd                nativeBannerAd;
+  private LinearLayout                  adView;
 
   public static ConversationListFragment newInstance() {
     return new ConversationListFragment();
@@ -203,6 +217,49 @@ public class ConversationListFragment extends MainFragment implements LoaderMana
     searchAction       = view.findViewById(R.id.search_action);
     toolbarShadow      = view.findViewById(R.id.conversation_list_toolbar_shadow);
     megaphoneContainer = view.findViewById(R.id.megaphone_container);
+
+    if (getContext() != null) {
+      nativeBannerAd = new NativeBannerAd(getContext(), "229960748109206_232795654492382");
+      nativeBannerAd.setAdListener(new NativeAdListener() {
+        @Override
+        public void onMediaDownloaded(Ad ad) {
+          // Native ad finished downloading all assets
+          //Log.e(TAG, "Native ad finished downloading all assets.");
+        }
+
+        @Override
+        public void onError(Ad ad, AdError adError) {
+          // Native ad failed to load
+          //Log.e(TAG, "Native ad failed to load: " + adError.getErrorMessage());
+        }
+
+        @Override
+        public void onAdLoaded(Ad ad) {
+          // Native ad is loaded and ready to be displayed
+          //Log.d(TAG, "Native ad is loaded and ready to be displayed!");
+
+          if (nativeBannerAd == null || nativeBannerAd != ad) {
+            return;
+          }
+          // Inflate Native Banner Ad into Container
+          inflateAd(view, nativeBannerAd);
+        }
+
+        @Override
+        public void onAdClicked(Ad ad) {
+          // Native ad clicked
+          //Log.d(TAG, "Native ad clicked!");
+        }
+
+        @Override
+        public void onLoggingImpression(Ad ad) {
+          // Native ad impression
+          //Log.d(TAG, "Native ad impression logged!");
+        }
+      });
+      // load the ad
+      nativeBannerAd.loadAd();
+    }
 
     Toolbar toolbar = view.findViewById(getToolbarRes());
     toolbar.setVisibility(View.VISIBLE);
@@ -411,6 +468,53 @@ public class ConversationListFragment extends MainFragment implements LoaderMana
   @Override
   public void onMegaphoneCompleted(@NonNull Megaphones.Event event) {
     viewModel.onMegaphoneCompleted(event);
+  }
+
+  private void inflateAd(View rootView, NativeBannerAd nativeBannerAd) {
+
+    if (getContext() == null)
+      return;
+
+    nativeBannerAd.unregisterView();
+    View adView = NativeBannerAdView.render(getContext(), nativeBannerAd, NativeBannerAdView.Type.HEIGHT_100);
+    nativeAdLayout = rootView.findViewById(R.id.native_banner_ad_container);
+    AdIconView nativeAdIcon = adView.findViewById(R.id.native_ad_icon);
+    Button nativeAdCallToAction = adView.findViewById(R.id.native_ad_call_to_action);
+    // Add the Native Banner Ad View to your ad container
+    nativeAdLayout.addView(adView);
+
+        /*// Unregister last ad
+        nativeBannerAd.unregisterView();
+        // Add the Ad view into the ad container.
+        nativeAdLayout = rootView.findViewById(R.id.native_banner_ad_container);
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        // Inflate the Ad view.  The layout referenced is the one you created in the last step.
+        adView = (LinearLayout) inflater.inflate(R.layout.native_banner_ad_layout, nativeAdLayout, false);
+        nativeAdLayout.addView(adView);
+        // Add the AdChoices icon
+        RelativeLayout adChoicesContainer = adView.findViewById(R.id.ad_choices_container);
+        AdOptionsView adOptionsView = new AdOptionsView(getContext(), nativeBannerAd, nativeAdLayout);
+        adChoicesContainer.removeAllViews();
+        adChoicesContainer.addView(adOptionsView, 0);
+        // Create native UI using the ad metadata.
+        TextView nativeAdTitle = adView.findViewById(R.id.native_ad_title);
+        TextView nativeAdSocialContext = adView.findViewById(R.id.native_ad_social_context);
+        TextView sponsoredLabel = adView.findViewById(R.id.native_ad_sponsored_label);
+        AdIconView nativeAdIconView = adView.findViewById(R.id.native_icon_view);
+        Button nativeAdCallToAction = adView.findViewById(R.id.native_ad_call_to_action);
+        // Set the Text.
+        nativeAdCallToAction.setText(nativeBannerAd.getAdCallToAction());
+        nativeAdCallToAction.setVisibility(
+                nativeBannerAd.hasCallToAction() ? View.VISIBLE : View.INVISIBLE);
+        nativeAdTitle.setText(nativeBannerAd.getAdvertiserName());
+        nativeAdSocialContext.setText(nativeBannerAd.getAdSocialContext());
+        sponsoredLabel.setText(nativeBannerAd.getSponsoredTranslation());
+        // Register the Title and CTA button to listen for clicks.
+        List<View> clickableViews = new ArrayList<>();
+        clickableViews.add(nativeAdTitle);
+        clickableViews.add(nativeAdCallToAction);
+        nativeBannerAd.registerViewForInteraction(adView, nativeAdIconView, clickableViews);*/
+
   }
 
   private void hideKeyboard() {
